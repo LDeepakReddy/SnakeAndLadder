2c0e5d8e549d1162d6d6597fc2139d3904faca08
LDeepakReddy <l.deepakreddy.com>
2022-02-23 16:07:33 +0530

    Die Rolled

--- src/com/blz/SnakeAndLadder.java
+++ src/com/blz/SnakeAndLadder.java
@@ -4,9 +4,13 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int START_POSITION = 0;
+
         System.out.println("welcome to the snake and ladder program");
         System.out.println("Player started at " + START_POSITION);
 
+        //Rolling Dice
+        int diceNumber=(int) Math.floor(Math.random()*10) % 6 + 1;
+        System.out.println("Die Rolled : " +diceNumber);
 
     }
 }

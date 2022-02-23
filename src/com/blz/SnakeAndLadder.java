41f92b360d4831fa36b0fb4d5ed911b936b9a11c
LDeepakReddy <l.deepakreddy.com>
2022-02-23 16:02:38 +0530

    Player initial position

--- src/com/blz/SnakeAndLadder.java
+++ src/com/blz/SnakeAndLadder.java
@@ -3,8 +3,10 @@ package com.blz;
 public class SnakeAndLadder {
 
     public static void main(String[] args) {
-
+        int START_POSITION = 0;
         System.out.println("welcome to the snake and ladder program");
+        System.out.println("Player started at " + START_POSITION);
+
 
     }
 }

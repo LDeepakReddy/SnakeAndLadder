98cce3a4e47c4afadc9e718d89190ee199c4e404
LDeepakReddy <l.deepakreddy.com>
2022-02-23 16:44:50 +0530

    Repeating till reaches 100

--- src/com/blz/SnakeAndLadder.java
+++ src/com/blz/SnakeAndLadder.java
@@ -7,21 +7,29 @@ public class SnakeAndLadder {
         int IS_SNAKE = 1;
         int IS_LADDER = 2;
         int START_POSITION = 0;
+        int WINNING_POSITION = 100;
         int CurrentPosition = START_POSITION;
 
         System.out.println("welcome to the snake and ladder program");
-        //Rolling Dice
-        int diceNumber=(int) Math.floor(Math.random()*10) % 6 + 1;
-        //check for options
-        int option = (int) Math.floor(Math.random() * 10) % 3;
-        if (option == IS_SNAKE) {
-            CurrentPosition = CurrentPosition - diceNumber;
-            System.out.println("snake option");
-        }else if (option == IS_LADDER) {
-            CurrentPosition = CurrentPosition + diceNumber;
-            System.out.println("ladder option");
-        }else {
-            System.out.println("no play");
+        while (CurrentPosition < WINNING_POSITION) {
+            //Rolling Dice
+            int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            //check for options
+            int option = (int) Math.floor(Math.random() * 10) % 3;
+            if (option == IS_SNAKE) {
+
+                if (CurrentPosition < 0) {
+                    CurrentPosition = START_POSITION;
+                } else {
+                    CurrentPosition = CurrentPosition - diceNumber;
+                    System.out.println("snake option at " + CurrentPosition);
+                }
+            } else if (option == IS_LADDER) {
+                CurrentPosition = CurrentPosition + diceNumber;
+                System.out.println("ladder option at " + CurrentPosition);
+            } else {
+                System.out.println("no play at " + CurrentPosition);
+            }
         }
         System.out.println("Player Current position is " + CurrentPosition);
 

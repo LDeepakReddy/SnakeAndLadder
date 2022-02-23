02fc698253674b07868a5bbefee2d582fc79c596
LDeepakReddy <l.deepakreddy.com>
2022-02-23 15:57:28 +0530

    welcome message to the snake and ladder program

---
+++ src/com/blz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.blz;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        System.out.println("welcome to the snake and ladder program");
+
+    }
+}
